454bf3ac85857d6f69e66cb0f34582ed23fea7ed
Ubuntu <ubuntu@ip-172-26-11-79.us-east-2.compute.internal>
2018-12-26 09:07:13 -0500

    lightsail init updates

--- lightsail-init.sh
+++ lightsail-init.sh
@@ -6,27 +6,42 @@ name=$(uname -a)
 npm_command=npm
 
 if [[ name =~ "Ubuntu" ]]; then
-	# neovim
-	sudo add-apt-repository ppa:neovim-ppa/stable
-	# fzf
-	sudo add-apt-repository ppa:fmarier/ppa
-	sudo apt update
-	sudo apt install neovim
-	sudo apt install fzf
-	sudo apt install zsh
-	sudo apt install nodejs
-	sudo apt install npm
-	npm_command=sudo npm
+    # neovim
+    sudo add-apt-repository ppa:neovim-ppa/stable
+    # fzf
+    sudo add-apt-repository ppa:fmarier/ppa
+
+    # docker
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+    # nodejs
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+
+    sudo apt update
+
+    # general utilities
+    sudo apt install -y neovim fzf zsh nodejs npm
+    
+    # docker
+    sudo apt install -y \
+        apt-transport-https \
+        ca-certificates curl \
+        software-properties-common \
+        docker-ce
+    sudo usermod -aG docker ${USER}
+
+    npm_command=sudo npm
 fi
 
 $npm_command install -g npm
 
 if [[ ! -d .config ]]; then
-	mkdir .config
+    mkdir .config
 fi
 
 if [[ ! -d .dotfiles ]]; then
-	git clone https://github.com/jason0x43/dotfiles .dotfiles
+    git clone https://github.com/jason0x43/dotfiles .dotfiles
 fi
 
 ln -sr .dotfiles/home/zshrc .zshrc
