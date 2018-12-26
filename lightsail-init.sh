f78227debb9a6a21a0f46b4a9e905d315af78631
Ubuntu <ubuntu@ip-172-26-3-16.us-east-2.compute.internal>
2018-12-26 21:17:38 +0000

    Improve lightsail (vps) init

--- lightsail-init.sh
+++ lightsail-init.sh
@@ -1,37 +1,44 @@
-#!/usr/bin/bash
+#!/bin/bash
+
+set -e
 
 cd
 
 name=$(uname -a)
 npm_command=npm
 
-if [[ name =~ "Ubuntu" ]]; then
+if [[ $name =~ "Ubuntu" ]]; then
     # neovim
-    sudo add-apt-repository ppa:neovim-ppa/stable
+    sudo add-apt-repository -y ppa:neovim-ppa/stable
     # fzf
-    sudo add-apt-repository ppa:fmarier/ppa
+    sudo add-apt-repository -y ppa:fmarier/ppa
 
     # docker
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+    sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
     # nodejs
     curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 
-    sudo apt update
+    sudo apt-get update
 
     # general utilities
-    sudo apt install -y neovim fzf zsh nodejs npm
+    sudo apt-get install -y neovim fzf zsh nodejs
+
+    z_shell=$(which zsh)
+    if [[ -n $z_shell ]]; then
+        sudo chsh -s $z_shell $USER
+    fi
     
     # docker
-    sudo apt install -y \
+    sudo apt-get install -y \
         apt-transport-https \
         ca-certificates curl \
         software-properties-common \
         docker-ce
-    sudo usermod -aG docker ${USER}
+    sudo usermod -aG docker $USER
 
-    npm_command=sudo npm
+    npm_command="sudo npm"
 fi
 
 $npm_command install -g npm
@@ -40,8 +47,8 @@ if [[ ! -d .config ]]; then
     mkdir .config
 fi
 
-if [[ ! -d .dotfiles ]]; then
-    git clone https://github.com/jason0x43/dotfiles .dotfiles
+if [[ ! -d .local/share/vim/backup ]]; then
+    mkdir .local/share/vim/backup
 fi
 
 ln -sr .dotfiles/home/zshrc .zshrc
@@ -49,3 +56,4 @@ ln -sr .dotfiles/home/zshenv .zshenv
 ln -sr .dotfiles/home/tmux.conf .tmux.conf
 ln -sr .dotfiles/vim .vim
 ln -sr .dotfiles/vim .config/nvim
+ln -sr .dotfiles/home/gitconfig .gitconfig
