22d4b05992e587e1b9a047c7a68bc70f84da696e
Jason Cheatham <j.cheatham@gmail.com>
2019-05-29 10:44:39 -0400

    Add powershell git aliases

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,12 +1,15 @@
 if (test-path alias:gc) { remove-item -force alias:gc }
 if (test-path alias:gl) { remove-item -force alias:gl }
+if (test-path alias:gp) { remove-item -force alias:gp }
 
 function ga { git add $args }
 function gb { git branch $args }
 function gba { git branch -a $args }
 function gc { git commit $args }
+function gca { git commit -a $args }
 function gco { git checkout $args }
 function gd { git diff $args }
+function gf { git fetch $args }
 function gfpr {
     $id = $args[0]
     git fetch -fu origin refs/pull/$id/head:pr/$id
@@ -18,6 +21,7 @@ function gid { git rev-parse HEAD }
 # }
 function gl { git log --graph --abbrev-commit --date-order --format=format:'%Cblue%h%Creset%C(bold red)%d%Creset %s <%an> %Cgreen(%ar)%Creset' --all $args }
 function glb { git log --graph --abbrev-commit --date-order --format=format:'%Cblue%h%Creset%C(bold red)%d%Creset %s <%an> %Cgreen(%ar)%Creset' $args }
+function gp { git pull $args }
 function gri { git rebase -i  $args }
 function grv { git remote -v }
 function gs { git status --short $args }
