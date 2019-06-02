d10e3222cc4c43f749ca8b22c4ee9cdeaca3002d
Michelle Cheatham <michelle.cheatham@galois.com>
2019-06-02 17:44:04 -0400

    Added terminal config and missing node stuff

---
+++ zsh/npx.zsh
@@ -0,0 +1,19 @@
+
+command_not_found_handler() {
+  # Do not run within a pipe
+  if test ! -t 1; then
+    >&2 echo "command not found: $1"
+    return 127
+  fi
+  if which npx > /dev/null; then
+    echo "$1 not found. Trying with npx..." >&2
+  else
+    return 127
+  fi
+  if ! [[ $1 =~ @ ]]; then
+    npx --no-install "$@"
+  else
+    npx "$@"
+  fi
+  return $?
+}
