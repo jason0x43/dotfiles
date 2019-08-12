58e4233fc430d61877315de147417227bb4fb7a9
Jason Cheatham <j.cheatham@gmail.com>
2019-08-12 10:07:40 -0400

    Powershell updates
    Setup scoop

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -2,6 +2,8 @@ if (test-path alias:gc) { remove-item -force alias:gc }
 if (test-path alias:gl) { remove-item -force alias:gl }
 if (test-path alias:gp) { remove-item -force alias:gp }
 
+set-alias -name vi -value nvim
+
 function ga { git add $args }
 function gb { git branch $args }
 function gba { git branch -a $args }
@@ -16,9 +18,6 @@ function gfpr {
     git checkout pr/$id
 }
 function gid { git rev-parse HEAD }
-# function gfprc {
-#     pr-clean = "!git for-each-ref refs/heads/pr/* --format='%(refname)' | while read ref ; do branch=${ref#refs/heads/} ; git branch -D $branch ; done"
-# }
 function gl { git log --graph --abbrev-commit --date-order --format=format:'%Cblue%h%Creset%C(bold red)%d%Creset %s <%an> %Cgreen(%ar)%Creset' --all $args }
 function glb { git log --graph --abbrev-commit --date-order --format=format:'%Cblue%h%Creset%C(bold red)%d%Creset %s <%an> %Cgreen(%ar)%Creset' $args }
 function gp { git pull $args }
@@ -26,3 +25,10 @@ function gri { git rebase -i  $args }
 function grv { git remote -v }
 function gs { git status --short $args }
 function which { get-command $args }
+
+$scoopPath = $env:USERPROFILE + '\scoop\shims'
+$cleanedPath = ($env:PATH.split(';')) -notmatch ($scoopPath -replace '\\', '\\')
+$env:PATH = (@($scoopPath) + $cleanedPath) -join ';'
+$env:GIT_SSH = (scoop which ssh) 
+
+import-module ~\scoop\modules\posh-git
