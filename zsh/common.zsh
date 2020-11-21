a5983204ce7789151947a2e39a5ebe1cfd80c651
Jason Cheatham <jason@jasoncheatham.com>
2020-11-20 22:40:39 -0500

    Move common env var definitions to common.zsh

---
+++ zsh/common.zsh
@@ -0,0 +1,20 @@
+export DOTFILES=$HOME/.dotfiles
+export CACHEDIR=$HOME/.cache
+export CONFIGDIR=$HOME/.config
+export DATADIR=$HOME/.local/share
+
+export ZCACHEDIR=$CACHEDIR/zsh
+export ZDATADIR=$DATADIR/zsh
+export ZPLUGDIR=$ZCACHEDIR/plugins
+export ZCOMPDIR=$ZCACHEDIR/completions
+export ZFUNCDIR=$ZDATADIR/functions
+
+export ASDF_DATA=$HOME/.asdf
+
+if [[ -d /home/linuxbrew ]]; then
+    export HOMEBREW_BASE=/home/linuxbrew/.linuxbrew
+elif [[ -d $HOME/.linuxbrew ]]; then
+    export HOMEBREW_BASE=$HOME/.linuxbrew
+else
+    export HOMEBREW_BASE=/usr/local
+fi
