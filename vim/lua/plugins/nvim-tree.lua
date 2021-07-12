11414d6f880f1af04e55494b2110d3a1c3b0fc65
Jason Cheatham <jason@jasoncheatham.com>
2021-07-11 22:57:44 -0400

    Auto-close nvim tree

--- vim/lua/plugins/nvim-tree.lua
+++ vim/lua/plugins/nvim-tree.lua
@@ -1,14 +1,20 @@
 local util = require('util')
 local g = vim.g
 
+-- follow currently open file in tree
+g.nvim_tree_follow = 1
+
 -- show hierarchy lines
 g.nvim_tree_indent_markers = 1
 
 -- append a slash to folder names
 g.nvim_tree_add_trailing = 1
 
--- close the tree after opening a file
+-- close the tree if it's the only open buffer
 g.nvim_tree_auto_close = 1
+
+-- close the tree after opening a file
+g.nvim_tree_quit_on_open = 1
 
 -- open the tree on the right side
 g.nvim_tree_side = 'right'
@@ -16,4 +22,4 @@ g.nvim_tree_side = 'right'
 -- wider tree
 g.nvim_tree_width = '30%'
 
-util.keys.lmap('n', ':NvimTreeFindFile<CR>')
+util.keys.lmap('n', ':NvimTreeToggle<CR>')
