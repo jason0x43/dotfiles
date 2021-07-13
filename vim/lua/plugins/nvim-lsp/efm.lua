95ce55e72b910465d52e1bddeb0daf8c2e787e77
Jason Cheatham <jason@jasoncheatham.com>
2021-07-13 09:51:57 -0400

    Switch to stylua for lua formatting

--- vim/lua/plugins/nvim-lsp/efm.lua
+++ vim/lua/plugins/nvim-lsp/efm.lua
@@ -8,41 +8,21 @@ if vim.fn.executable('prettier') == 0 then
   print('WARNING: Missing prettier')
 end
 
-if vim.fn.executable('lua-format') == 0 then
-  print('WARNING: Missing lua-format')
+if vim.fn.executable('stylua') == 0 then
+  print('WARNING: Missing stylua')
 end
 
 local eslint = {
   lintCommand = 'eslint_d -f unix --stdin --stdin-filename ${INPUT}',
   lintStdin = true,
-  lintIgnoreExitCode = true
+  lintIgnoreExitCode = true,
 }
 
 local prettier = { formatCommand = 'prettier' }
 
-local lua_opts = {
-  '-i',
-  '--column-limit=80',
-  '--continuation-indent-width=2',
-  '--indent-width=2',
-  '--no-align-args',
-  '--no-align-parameter',
-  '--break-after-functiondef-lp',
-  '--break-before-functiondef-rp',
-  '--break-after-functioncall-lp',
-  '--break-before-functioncall-rp',
-  '--chop-down-parameter',
-  '--chop-down-table',
-  '--double-quote-to-single-quote',
-  '--no-keep-simple-control-block-one-line',
-  '--no-keep-simple-function-one-line',
-  '--spaces-around-equals-in-field',
-  '--spaces-inside-table-braces'
-}
-
-local lua_format = {
-  formatCommand = 'lua-format ' .. table.concat(lua_opts, ' '),
-  formatStdin = true
+local stylua = {
+  formatCommand = 'stylua --stdin-filepath ${INPUT} --search-parent-directories -',
+  formatStdin = true,
 }
 
 exports.config = {
@@ -67,9 +47,9 @@ exports.config = {
       typecriptreact = { eslint, prettier },
       markdown = { prettier },
       html = { prettier },
-      lua = { lua_format }
-    }
-  }
+      lua = { stylua },
+    },
+  },
 }
 
 return exports
