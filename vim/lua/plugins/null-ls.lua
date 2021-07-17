ce0231d049634e70728576ca60f475745b771c4e
Jason Cheatham <jason@jasoncheatham.com>
2021-07-17 17:44:36 -0400

    Fix stylua argument setup for null-ls

--- vim/lua/plugins/null-ls.lua
+++ vim/lua/plugins/null-ls.lua
@@ -2,6 +2,7 @@ local exports = {}
 
 function exports.setup()
   local null_ls = require('null-ls')
+
   null_ls.setup({
     sources = {
       null_ls.builtins.formatting.prettierd.with({
@@ -9,12 +10,12 @@ function exports.setup()
         filetypes = { 'markdown', 'html', 'json', 'yaml' },
       }),
       null_ls.builtins.formatting.stylua.with({
-        arguments = {
+        args = {
           '--stdin-filepath',
           '$FILENAME',
           '--search-parent-directories',
           '-',
-        },
+        }
       }),
     },
     on_attach = require('lsp').on_attach,
