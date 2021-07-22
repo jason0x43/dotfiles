687c49b3f6792ff373f1d53388006ac53c5a16b2
Jason Cheatham <jason@jasoncheatham.com>
2021-07-22 11:35:20 -0400

    Remove duplicate nvim-tree keymap

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -1,4 +1,3 @@
-local util = require('util')
 local g = vim.g
 
 -- follow currently open file in tree
@@ -21,5 +20,3 @@ g.nvim_tree_side = 'right'
 
 -- wider tree
 g.nvim_tree_width = '30%'
-
-util.keys.lmap('n', ':NvimTreeToggle<CR>')
