d41bba0217ac66a21a2e070f550d363c398f3d75
Jason Cheatham <jason@jasoncheatham.com>
2021-07-24 09:08:27 -0400

    Disable shadafile during init

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,11 @@
+-- disable shadafile while sourcing config
+-- https://www.reddit.com/r/neovim/comments/opipij/guide_tips_and_tricks_to_reduce_startup_and/
+vim.opt.shadafile = 'NONE'
+
 require('config.options')
 require('config.keymaps')
 require('config.autocommands')
 require('plugins')
 require('theme')
+
+vim.opt.shadafile = ''
