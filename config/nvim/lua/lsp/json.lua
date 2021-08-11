8932517ae8efa01c7ccb81a616eb3e81e4cc5e40
Jason Cheatham <jason@jasoncheatham.com>
2021-08-10 20:11:57 -0400

    Disable json LSP formatting in favor of null-ls

--- config/nvim/lua/lsp/json.lua
+++ config/nvim/lua/lsp/json.lua
@@ -2,4 +2,9 @@ local exports = {}
 
 exports.config = { filetypes = { 'json', 'jsonc' } }
 
+function exports.on_attach(client)
+  -- disable formatting for JSON; we'll use prettier through null-ls instead
+  client.resolved_capabilities.document_formatting = false
+end
+
 return exports
