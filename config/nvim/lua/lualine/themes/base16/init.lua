2e1cb975e6d8b74b6800b3257a6d66e05b0e564a
Jason Cheatham <jason@jasoncheatham.com>
2021-08-22 15:59:24 -0400

    Update base16 theme for shadmansaleh/lualine update

--- config/nvim/lua/lualine/themes/base16/init.lua
+++
@@ -1,5 +0,0 @@
-require('util').augroup('init_lualine_base16', {
-  'ColorScheme * lua require("lualine.themes.base16.util").update_colors()'
-})
-
-return require('lualine.themes.base16.util').get_theme()
