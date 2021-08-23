cd80db22296dc07b1210bbe8db5bf7e31de5a022
Jason Cheatham <jason@jasoncheatham.com>
2021-08-23 09:13:42 -0400

    Add 'hs' lua global

--- config/nvim/lua/lsp/lua.lua
+++ config/nvim/lua/lsp/lua.lua
@@ -1,7 +1,9 @@
 local exports = {}
 
 exports.config = {
-  settings = { Lua = { diagnostics = { globals = { 'vim', 'packer_plugins' } } } },
+  settings = {
+    Lua = { diagnostics = { globals = { 'hs', 'vim', 'packer_plugins' } } },
+  },
 }
 
 return exports
