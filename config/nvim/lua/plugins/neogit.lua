c8e1a9607a33fedcdb846bb795f8b2a149ece62f
Jason Cheatham <jason@jasoncheatham.com>
2021-09-03 10:47:47 -0400

    Formatting

--- config/nvim/lua/plugins/neogit.lua
+++ config/nvim/lua/plugins/neogit.lua
@@ -5,8 +5,14 @@ require('neogit').setup({
 local theme = require('util.theme')
 local c = theme.get_colors()
 
-theme.hi('NeogitFoldOpen', { fg=c('fg_sign'), bg=c('bg_sign'), style='bold' })
-theme.hi('NeogitFoldClosed', { fg=c('fg_sign'), bg=c('bg_sign'), style='bold' })
+theme.hi(
+  'NeogitFoldOpen',
+  { fg = c('fg_sign'), bg = c('bg_sign'), style = 'bold' }
+)
+theme.hi(
+  'NeogitFoldClosed',
+  { fg = c('fg_sign'), bg = c('bg_sign'), style = 'bold' }
+)
 
 vim.cmd('sign define NeogitOpen:section texthl=NeogitFoldOpen')
 vim.cmd('sign define NeogitClosed:section texthl=NeogitFoldClosed')
