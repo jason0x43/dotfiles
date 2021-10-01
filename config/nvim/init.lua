115ebf58e595fb9693ccf4cbfc2c608bb63a85ee
Jason Cheatham <jason@jasoncheatham.com>
2021-10-01 15:41:21 -0400

    Show a message if impatient doesn't load

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,7 +2,10 @@
 -- https://www.reddit.com/r/neovim/comments/opipij/guide_tips_and_tricks_to_reduce_startup_and/
 vim.opt.shadafile = 'NONE'
 
-pcall(require, 'impatient')
+local status = pcall(require, 'impatient')
+if not status then
+  print('Failed to load impatient')
+end
 
 require('config.packer')
 require('config.options')
