39c1f2a12e554c170eefa1ceaf70d15b00c21ebf
Jason Cheatham <jason@jasoncheatham.com>
2021-10-01 16:02:36 -0400

    Enable treesitter indenting for all but ts/js

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -6,17 +6,19 @@ local config = {
     -- this should be set to a list of filetypes, but that doesn't work
     -- https://github.com/nvim-treesitter/nvim-treesitter#modules
     -- additional_vim_regex_highlighting = true
+    additional_vim_regex_highlighting = require('util').ts_types,
   },
   indent = {
-    enable = false,
+    enable = true,
     -- indenting is currently broken for several languages, particularly for doc
     -- comments
-    disable = vim.list_extend({
-      'cpp',
-      'python',
-    }, require(
-      'util'
-    ).ts_types),
+    -- disable = vim.list_extend({
+    --   'cpp',
+    --   'python',
+    -- }, require(
+    --   'util'
+    -- ).ts_types),
+    disable = require('util').ts_types,
   },
   matchup = {
     enable = true,
