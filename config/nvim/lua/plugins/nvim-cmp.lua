9a554e0066e3a9093125634f6ecf88f6a7f77e08
Jason Cheatham <jason@jasoncheatham.com>
2021-10-12 21:24:50 -0400

    Simplify cmp key handling to try to stop undo history breaks

--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,25 +1,54 @@
 local cmp = require('cmp')
-local luasnip = require('luasnip')
 
-vim.o.completeopt = 'menuone,noselect'
+-- local has_words_before = function()
+--   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+--   return col ~= 0
+--     and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+--         :sub(col, col)
+--         :match('%s')
+--       == nil
+-- end
+
+vim.opt.completeopt = { 'menuone', 'noselect' }
 
 cmp.setup({
   snippet = {
     expand = function(args)
-      luasnip.lsp_expand(args.body)
+      require('luasnip').lsp_expand(args.body)
     end,
   },
   mapping = {
-    ['<cr>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
+    ['<c-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<c-f>'] = cmp.mapping.scroll_docs(4),
+    ['<c-e>'] = cmp.mapping.close(),
+    ['<c-y>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     }),
-    ['<tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-    ['<s-tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+    -- ['<tab>'] = function(fallback)
+    --   if cmp.visible() then
+    --     cmp.select_next_item()
+    --   elseif luasnip.expand_or_jumpable() then
+    --     luasnip.expand_or_jump()
+    --   elseif has_words_before() then
+    --     cmp.complete()
+    --   else
+    --     cmp.complete()
+    --   end
+    -- end,
+    -- ['<s-tab>'] = function(fallback)
+    --   if cmp.visible() then
+    --     cmp.select_prev_item()
+    --   elseif luasnip.jumpable(-1) then
+    --     luasnip.jump(-1)
+    --   else
+    --     fallback()
+    --   end
+    -- end,
   },
   sources = {
-    { name = 'nvim_lsp' },
     { name = 'nvim_lua' },
+    { name = 'nvim_lsp' },
     { name = 'path' },
     { name = 'buffer' },
   },
