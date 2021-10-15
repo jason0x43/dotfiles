5f4cba3283dbf00fe222ada1c6deb6dfa1f0085e
Jason Cheatham <jason@jasoncheatham.com>
2021-10-15 15:59:03 -0400

    Make Navigator mappings work only in normal mode

--- config/nvim/lua/plugins/Navigator.lua
+++ config/nvim/lua/plugins/Navigator.lua
@@ -2,7 +2,7 @@ local util = require('util')
 
 require('Navigator').setup()
 
-util.map('<C-j>', '<cmd>lua require("Navigator").down()<cr>')
-util.map('<C-h>', '<cmd>lua require("Navigator").left()<cr>')
-util.map('<C-k>', '<cmd>lua require("Navigator").up()<cr>')
-util.map('<C-l>', '<cmd>lua require("Navigator").right()<cr>')
+util.nmap('<C-j>', '<cmd>lua require("Navigator").down()<cr>')
+util.nmap('<C-h>', '<cmd>lua require("Navigator").left()<cr>')
+util.nmap('<C-k>', '<cmd>lua require("Navigator").up()<cr>')
+util.nmap('<C-l>', '<cmd>lua require("Navigator").right()<cr>')
