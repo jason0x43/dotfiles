ff0134f877aa27735c8b932dbe7a8429f34b591a
Jason Cheatham <jason@jasoncheatham.com>
2021-10-19 08:46:28 -0400

    Make normal floats have a higher-contrast background

--- config/nvim/lua/colors/selenized.lua
+++ config/nvim/lua/colors/selenized.lua
@@ -238,7 +238,7 @@ function exports.apply_theme(theme_name)
   hi('Pmenu', palette.fg_0, palette.bg_1)
 
   -- neovim
-  hi('NormalFloat', nil, palette.bg_2, nil, nil)
+  hi('NormalFloat', nil, palette.bg_0, nil, nil)
   hi_link('FloatBorder', 'NormalFloat')
 
   -- override some of the default treesitter links
