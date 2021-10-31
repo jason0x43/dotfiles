b4c6c23e2fc9391041a1f8715f86e62d723925dd
Jason Cheatham <jason@jasoncheatham.com>
2021-10-31 13:00:48 -0400

    Make cmp config handle missing cmp

--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,16 +1,20 @@
-local cmp = require('cmp')
-local luasnip = require('luasnip')
+local srequire = require('util').srequire
+local cmp = srequire('cmp')
+
+if cmp == nil then
+  return
+end
 
 vim.opt.completeopt = { 'menuone', 'noselect' }
 
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0
-    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
-        :sub(col, col)
-        :match('%s')
-      == nil
-end
+-- local has_words_before = function()
+--   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+--   return col ~= 0
+--     and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+--         :sub(col, col)
+--         :match('%s')
+--       == nil
+-- end
 
 cmp.setup({
   experimental = {
