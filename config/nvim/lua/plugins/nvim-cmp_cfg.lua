a20e417df88b3b4b204b067c8573388dab2a61f0
Jason Cheatham <jason@jasoncheatham.com>
2021-11-04 16:50:27 -0400

    Get snippets working properly with nvim-cmp

--- config/nvim/lua/plugins/nvim-cmp_cfg.lua
+++ config/nvim/lua/plugins/nvim-cmp_cfg.lua
@@ -1,20 +1,12 @@
 local srequire = require('util').srequire
 local cmp = srequire('cmp')
+local luasnip = srequire('luasnip')
 
 if cmp == nil then
   return
 end
 
 vim.opt.completeopt = { 'menuone', 'noselect' }
-
--- local has_words_before = function()
---   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
---   return col ~= 0
---     and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
---         :sub(col, col)
---         :match('%s')
---       == nil
--- end
 
 cmp.setup({
   experimental = {
@@ -24,44 +16,27 @@ cmp.setup({
   },
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+      print('expanding')
+      luasnip.lsp_expand(args.body)
     end,
   },
   mapping = {
-    ['<c-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<c-f>'] = cmp.mapping.scroll_docs(4),
-    ['<c-e>'] = cmp.mapping.close(),
-    ['<c-y>'] = cmp.mapping.confirm({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<C-y>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     }),
-    -- ['<cr>'] = function(fallback)
-    --   if cmp.visible() then
-    --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<c-y>'))
-    --   else
-    --     fallback()
-    --   end
-    -- end,
-    -- ['<tab>'] = function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_next_item()
-    --   elseif luasnip.expand_or_jumpable() then
-    --     luasnip.expand_or_jump()
-    --   elseif has_words_before() then
-    --     cmp.complete()
-    --   else
-    --     fallback()
-    --   end
-    -- end,
-    -- ['<s-tab>'] = function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_prev_item()
-    --   elseif luasnip.jumpable(-1) then
-    --     luasnip.jump(-1)
-    --   else
-    --     fallback()
-    --   end
-    -- end,
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip and luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
   },
   sources = {
     { name = 'nvim_lua' },
