0d85eea0f41a3877efec0406b845b71a4ceaf045
Jason Cheatham <jason@jasoncheatham.com>
2021-11-06 10:33:36 -0400

    Add S-Tab mapping for cmp

--- config/nvim/lua/plugins/nvim-cmp_cfg.lua
+++ config/nvim/lua/plugins/nvim-cmp_cfg.lua
@@ -37,6 +37,13 @@ cmp.setup({
         fallback()
       end
     end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
   },
   sources = {
     { name = 'nvim_lua' },
