fb2ac9bc3d7cded91e60ce7ec0c3a4ec1170e93e
Jason Cheatham <jason@jasoncheatham.com>
2021-11-24 18:28:51 -0500

    Simplify denols config

--- config/nvim/lua/lsp/denols.lua
+++ config/nvim/lua/lsp/denols.lua
@@ -1,29 +1,15 @@
 local M = {}
 
-local lsp_find_root = require('lspconfig.util').root_pattern(
-  'package.json',
-  'deps.ts',
-  'mod.ts',
-  '.git'
-)
-
 M.config = {
-  root_dir = function(filename)
-    local dir = lsp_find_root(filename)
-    if dir ~= nil then
-      return dir
-    end
-
-    -- return the files path as the default root
-    return vim.fn.fnamemodify(filename, ':h')
-  end,
+  single_file_support = true,
 
   should_attach = function()
-    local filetype = vim.bo.filetype
-    return vim.fn.findfile('tsconfig.json', '.;') == ''
-      and vim.fn.findfile('jsconfig.json', '.;') == ''
-      and not filetype:find('react')
-      and not filetype:find('sx')
+    return vim.fn.findfile('deps.ts', '.;') ~= ''
+      or vim.fn.findfile('mod.ts', '.;') ~= ''
+      or (
+        vim.fn.findfile('tsconfig.json', '.;') == ''
+        and vim.fn.findfile('jsconfig.json', '.;') == ''
+      )
   end,
 }
 
