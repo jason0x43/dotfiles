30020fd01c402d4cc8be98af006112d0389cc4a2
Jason Cheatham <jason@jasoncheatham.com>
2021-11-28 16:29:07 -0500

    Enable deno linting, better goto definition

--- config/nvim/lua/lsp/denols.lua
+++ config/nvim/lua/lsp/denols.lua
@@ -3,6 +3,11 @@ local M = {}
 M.config = {
   single_file_support = true,
 
+  init_options = {
+    lint = true,
+    unstable = true,
+  },
+
   should_attach = function()
     return vim.fn.findfile('deps.ts', '.;') ~= ''
       or vim.fn.findfile('mod.ts', '.;') ~= ''
@@ -11,6 +16,16 @@ M.config = {
         and vim.fn.findfile('jsconfig.json', '.;') == ''
       )
   end,
+
+  handlers = {
+    ['textDocument/definition'] = function(err, result, ctx, config)
+      -- If denols returns multiple results, goto the first local one
+      if #result > 1 then
+        result = { result[1] }
+      end
+      vim.lsp.handlers['textDocument/definition'](err, result, ctx, config)
+    end,
+  },
 }
 
 return M
