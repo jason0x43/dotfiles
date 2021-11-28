76ff8b28f30077d92e2038ca49a9f75efd8f2b8c
Jason Cheatham <jason@jasoncheatham.com>
2021-11-28 16:29:45 -0500

    Hide a couple of annoying tsserver warnings

--- config/nvim/lua/lsp/tsserver.lua
+++ config/nvim/lua/lsp/tsserver.lua
@@ -4,12 +4,33 @@ local M = {}
 
 M.config = {
   handlers = {
-    ['textDocument/definition'] = function(err, result, ctx)
+    ['textDocument/definition'] = function(err, result, ctx, config)
       -- If tsserver returns multiple results, ignore all but the first
       if #result > 1 then
         result = { result[1] }
       end
-      vim.lsp.handlers['textDocument/definition'](err, result, ctx)
+      vim.lsp.handlers['textDocument/definition'](err, result, ctx, config)
+    end,
+
+    ['textDocument/publishDiagnostics'] = function(err, result, ctx, config)
+      result.diagnostics = vim.tbl_filter(function (diag)
+        -- ignore 80001 (file is a CommonJS module)
+        if diag.code == 80001 then
+          return false
+        end
+        -- ignore 80006 (this can be converted to an async function)
+        if diag.code == 80006 then
+          return false
+        end
+        return true
+      end, result.diagnostics)
+
+      vim.lsp.handlers['textDocument/publishDiagnostics'](
+        err,
+        result,
+        ctx,
+        config
+      )
     end,
   },
 
