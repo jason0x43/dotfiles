a85a5f3ec50ae83defae095dc50b7841911f8e7e
Jason Cheatham <jason@jasoncheatham.com>
2022-01-02 20:26:03 -0500

    Ignore pyright hints based on env var

--- config/nvim/lua/lsp/pyright.lua
+++ config/nvim/lua/lsp/pyright.lua
@@ -3,7 +3,28 @@ local M = {}
 M.config = {
   on_attach = function()
     require('util').bufcmd('OrganizeImports', 'PyrightOrganizeImports')
-  end
+  end,
+
+  handlers = {
+    ['textDocument/publishDiagnostics'] = function(err, result, ctx, config)
+      if vim.fn.getenv('PYRIGHT_HIDE_HINTS') == '1' then
+        result.diagnostics = vim.tbl_filter(function(diag)
+          -- ignore 'not accessed' diags
+          if diag.tags and diag.tags[1] ~= nil then
+            return false
+          end
+          return true
+        end, result.diagnostics)
+      end
+
+      vim.lsp.handlers['textDocument/publishDiagnostics'](
+        err,
+        result,
+        ctx,
+        config
+      )
+    end,
+  },
 }
 
 return M
