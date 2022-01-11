38084e96344ea2b3e77e2e837dd7cb2971b0c434
Jason Cheatham <jason@jasoncheatham.com>
2022-01-10 19:50:05 -0500

    Externalize alpha config, plugins cleanup

--- config/nvim/lua/user/plugins/init.lua
+++ config/nvim/lua/user/plugins/init.lua
@@ -68,61 +68,7 @@ packer.startup({
       'goolord/alpha-nvim',
       requires = { 'kyazdani42/nvim-web-devicons' },
       config = function()
-        require('user.req')('alpha.themes.startify', function(startify)
-          startify.section.header.val = {
-            ' ____ ____ ____ ____ ____ ____ ',
-            '||n |||e |||o |||v |||i |||m ||',
-            '||__|||__|||__|||__|||__|||__||',
-            '|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|',
-          }
-
-          -- switch the mru and mru_cwd section order
-          startify.opts.layout[5] = startify.section.mru_cwd
-          startify.opts.layout[6] = startify.section.mru
-
-          table.insert(startify.opts.layout, 7, {
-            type = 'padding',
-            val = 1,
-          })
-
-          startify.section.bookmarks = {
-            type = 'group',
-            val = {
-              {
-                type = 'text',
-                val = 'Bookmarks',
-                opts = { hl = 'SpecialComment' },
-              },
-              { type = 'padding', val = 1 },
-              {
-                type = 'group',
-                val = {
-                  startify.file_button(
-                    '~/.config/nvim/lua/user/plugins/init.lua',
-                    'c'
-                  ),
-                  startify.file_button('~/.zshrc', 'z'),
-                },
-              },
-            },
-          }
-
-          table.insert(startify.opts.layout, 8, startify.section.bookmarks)
-
-          -- update the title of the mru section and only show 5 items
-          startify.section.mru.val[2].val = 'Recent'
-          startify.section.mru.val[4].val = function()
-            return { startify.mru(5, nil, 5) }
-          end
-
-          -- update the title of the mru_cwd section and only show 5 items
-          startify.section.mru_cwd.val[2].val = 'Recent (cwd)'
-          startify.section.mru_cwd.val[4].val = function()
-            return { startify.mru(0, vim.fn.getcwd(), 5) }
-          end
-
-          require('alpha').setup(startify.opts)
-        end)
+        require('user.plugins.alpha_cfg')
       end,
     })
 
@@ -346,11 +292,6 @@ packer.startup({
     -- completion
     use({
       'hrsh7th/nvim-cmp',
-      -- disabled in favor of coq
-      -- disable = true,
-      config = function()
-        require('user.plugins.nvim-cmp_cfg')
-      end,
       requires = {
         'L3MON4D3/LuaSnip',
         'hrsh7th/cmp-buffer',
@@ -359,39 +300,9 @@ packer.startup({
         'hrsh7th/cmp-nvim-lsp',
         'saadparwaiz1/cmp_luasnip',
       },
-    })
-
-    -- completion
-    use({
-      'ms-jpq/coq_nvim',
-      branch = 'coq',
-      run = ':COQdeps',
-      disable = true,
-      setup = function()
-        vim.g.coq_settings = {
-          auto_start = 'shut-up',
-          keymap = {
-            jump_to_mark = '<c-i>',
-          },
-          display = {
-            icons = { mode = 'none' },
-            ghost_text = { enabled = false },
-            pum = { source_context = { '', '' } },
-            preview = {
-              positions = {
-                north = 0,
-                south = 0,
-                west = 0,
-                east = 0,
-              },
-            },
-          },
-        }
+      config = function()
+        require('user.plugins.nvim-cmp_cfg')
       end,
-      requires = {
-        { 'ms-jpq/coq.thirdparty', branch = '3p' },
-        { 'ms-jpq/coq.artifacts', branch = 'artifacts' },
-      },
     })
 
     -- startup time profiling
