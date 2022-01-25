235435c69763dc2bc5e654baa480926d6969cf16
Jason Cheatham <jason@jasoncheatham.com>
2022-01-25 12:16:50 -0500

    Add deno config schema to jsonls

--- config/nvim/lua/user/lsp/jsonls.lua
+++ config/nvim/lua/user/lsp/jsonls.lua
@@ -6,15 +6,21 @@ M.config = {
   on_attach = function(client)
     -- disable formatting for JSON; we'll use prettier through null-ls instead
     client.resolved_capabilities.document_formatting = false
-  end
+  end,
 }
 
 local schemastore = require('user.req')('schemastore')
 if schemastore then
   M.config.settings = {
     json = {
-      schemas = schemastore.json.schemas()
-    }
+      schemas = vim.list_extend({
+        {
+          description = 'Deno configuration file',
+          fileMatch = { 'deno.json', 'deno.jsonc' },
+          url = 'https://raw.githubusercontent.com/denoland/deno/main/cli/schemas/config-file.v1.json',
+        },
+      }, schemastore.json.schemas()),
+    },
   }
 end
 
