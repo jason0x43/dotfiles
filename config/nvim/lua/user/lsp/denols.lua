327596c1d2b17817e82bf2a5836091aa26026ccb
Jason Cheatham <jason@jasoncheatham.com>
2022-03-12 21:02:33 -0500

    Sort deno code actions
    Prioritize import actions, deprioritize ignore / disable actions

--- config/nvim/lua/user/lsp/denols.lua
+++ config/nvim/lua/user/lsp/denols.lua
@@ -33,11 +33,59 @@ M.config = {
         config
       )
     end,
+
+    ['textDocument/codeAction'] = function(err, result, ctx, config)
+      print('handling deno codeaction')
+      denols.default_config.handlers['textDocument/codeAction'](
+        err,
+        result,
+        ctx,
+        config
+      )
+    end,
   },
 }
 
 M.start = lsp_util.create_start('denols')
 
 lsp_util.create_autostart_autocmd('denols', require('user.util').ts_types)
+
+local function score_code_action(result)
+  local title = result.title
+  -- imports first
+  if title:find('Import ') == 1 then
+    return 1
+  end
+  -- disable/ignore last
+  if title:find('Disable ') == 1 or title:find('Ignore ') == 1 then
+    return 3
+  end
+  return 2
+end
+
+-- Override buf_request_sync to sort codeAction results before they get to
+-- Telescope
+local buf_request_sync = vim.lsp.buf_request_sync
+vim.lsp.buf_request_sync = function(bufnr, method, params, timeout_ms)
+  local results, err = buf_request_sync(bufnr, method, params, timeout_ms)
+  if method == 'textDocument/codeAction' and err == nil then
+    for client_id, response in pairs(results) do
+      if response.result then
+        local client = vim.lsp.get_client_by_id(client_id)
+        if client.name == 'denols' then
+          table.sort(response.result, function(a, b)
+            local aScore = score_code_action(a)
+            local bScore = score_code_action(b)
+            if aScore == bScore then
+              return a.title < b.title
+            end
+            return aScore < bScore
+          end)
+        end
+      end
+    end
+  end
+  return results, err
+end
 
 return M
