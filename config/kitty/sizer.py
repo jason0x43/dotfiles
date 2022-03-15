30e5a4b7ab8467b5066b8dfb2aec796cfa4e0da1
Jason Cheatham <jason@jasoncheatham.com>
2022-03-15 09:13:34 -0400

    Improve kitty window sizing shortcuts

--- config/kitty/sizer.py
+++ config/kitty/sizer.py
@@ -1,14 +1,20 @@
-# Handle navigating between various splits (vim, kitty, tmux)
+# Handle resizing windows (panes)
 
-from typing import Any, Sequence, cast
-from collections import namedtuple
+from typing import Any, Dict, Optional, Sequence
 
 from kittens.tui.handler import result_handler
 from kittens.tui.loop import debug
-from kitty.typing import BossType, EdgeLiteral
-from kitty.types import WindowGeometry
-from kitty.window import Window
-from  kitty.options.utils import resize_window
+from kitty.typing import BossType
+
+
+def get_split_type(window_id: int, layout_pair: Dict[str, Any]) -> Optional[str]:
+    if layout_pair['one'] == window_id or layout_pair['two'] == window_id:
+        return 'horizontal' if layout_pair['horizontal'] else 'vertical'
+    if isinstance(layout_pair['one'], dict):
+        return get_split_type(window_id, layout_pair['one'])
+    if isinstance(layout_pair['two'], dict):
+        return get_split_type(window_id, layout_pair['two'])
+    return None
 
 
 @result_handler(no_ui=True)
@@ -19,6 +25,15 @@ def handle_result(
 
     tab = boss.active_tab
     if tab:
+        layout = tab.current_layout.layout_state()
+        if quality == 'bigger' or quality == 'smaller':
+            split_type = get_split_type( target_window_id, layout['pairs'])
+            if split_type is None:
+                return
+            if split_type == 'horizontal':
+                quality = 'wider' if quality == 'bigger' else 'narrower'
+            else:
+                quality = 'taller' if quality == 'bigger' else 'shorter'
         tab.resize_window(quality, 1)
 
 
