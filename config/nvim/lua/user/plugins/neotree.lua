89e0000d56cd5053e49990299a4b1b35dbadb4c0
Jason Cheatham <jason@jasoncheatham.com>
2022-08-24 08:18:00 -0400

    Close neotree when opening a file

--- config/nvim/lua/user/plugins/neotree.lua
+++ config/nvim/lua/user/plugins/neotree.lua
@@ -9,6 +9,14 @@ M.config = function()
     popup_border_style = 'solid',
     window = {
       position = 'right'
+    },
+    event_handlers = {
+      {
+        event = 'file_opened',
+        handler = function()
+          require('neo-tree').close_all()
+        end
+      }
     }
   })
 
