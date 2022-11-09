06d6ca2b5a2f8fcfac2e9cc5c1a32854abb45acf
Jason Cheatham <jason@jasoncheatham.com>
2022-11-09 15:25:34 -0500

    Setup winbar support for lualine

--- config/nvim/lua/user/plugins/lualine.lua
+++ config/nvim/lua/user/plugins/lualine.lua
@@ -16,46 +16,37 @@ M.config = function()
       globalstatus = true,
     },
     sections = {
-      lualine_a = { 'mode' },
-      lualine_b = {
-        {
-          'filetype',
-          separator = '',
-          padding = { left = 0, right = 1 },
-          icon_only = true,
-        },
-      },
       lualine_c = {
         { 'filename', path = 1, padding = { left = 0, right = 1 } },
       },
+    },
+    winbar = {
+      lualine_c = {},
       lualine_x = {
         { 'diagnostics', sources = { 'nvim_diagnostic' } },
         {
           'language_servers',
           separator = '',
-          padding = { right = 0, left = 1 },
+          padding = { left = 1, right = 1 },
         },
         {
           'lsp_progress',
           display_components = { 'spinner' },
           spinner_symbols = { '⠖', '⠲', '⠴', '⠦' },
           timer = { spinner = 250 },
-          padding = { left = 1, right = 0 },
+          padding = { left = 1, right = 1 },
         },
       },
-      lualine_z = { 'location' },
     },
-    inactive_sections = {
-      lualine_c = { 'filename' },
-      lualine_x = { 'location' },
-    },
-    extensions = { 'nvim-tree', 'quickfix', 'symbols_outline' },
+    extensions = { 'nvim-tree', 'quickfix' },
   }
+
+  config.inactive_winbar = config.winbar
 
   local navic = req('nvim-navic')
   if navic then
     table.insert(
-      config.sections.lualine_c,
+      config.winbar.lualine_c,
       { navic.get_location, cond = navic.is_available }
     )
   end
