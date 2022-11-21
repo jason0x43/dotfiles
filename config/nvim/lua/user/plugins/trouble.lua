583134cf874c5a3991a3dee78981a138f3b3e071
Jason Cheatham <jason@jasoncheatham.com>
2022-11-21 11:02:14 -0500

    Go back to Telescope for diagnostics

--- config/nvim/lua/user/plugins/trouble.lua
+++ config/nvim/lua/user/plugins/trouble.lua
@@ -2,8 +2,8 @@ local M = {}
 
 M.config = function()
   require('trouble').setup()
-  require('user.util').lmap('ed', '<cmd>Trouble document_diagnostics<cr>')
-  require('user.util').lmap('ew', '<cmd>Trouble workspace_diagnostics<cr>')
+  -- require('user.util').lmap('ed', '<cmd>Trouble document_diagnostics<cr>')
+  -- require('user.util').lmap('ew', '<cmd>Trouble workspace_diagnostics<cr>')
 end
 
 return M
