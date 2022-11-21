6e20c32e0a668da2701f046b9f250e0a38111755
Jason Cheatham <jason@jasoncheatham.com>
2022-11-21 10:41:28 -0500

    Remove alpha bookmarks
    Never used

--- config/nvim/lua/user/plugins/alpha.lua
+++ config/nvim/lua/user/plugins/alpha.lua
@@ -13,44 +13,43 @@ M.config = function()
     '|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|',
   }
 
-  -- switch the mru and mru_cwd section order
   startify.opts.layout[5] = startify.section.mru_cwd
+  -- startify.opts.layout[6] = {
+  --   type = 'group',
+  --   val = {
+  --     { type = 'padding', val = 1 },
+  --     { type = 'text', val = 'Modified', opts = { hl = 'SpecialComment' } },
+  --     { type = 'padding', val = 1 },
+  --     {
+  --       type = 'group',
+  --       val = function()
+  --         local files = vim.fn.systemlist('git ls-files -m')
+  --         local buttons = {}
+  --         for i, f in ipairs(files) do
+  --           if i < 5 then
+  --             local file_button = startify.file_button(f, tostring(i + 4), f)
+  --             buttons[i] = file_button
+  --           end
+  --         end
+  --         return buttons
+  --       end,
+  --     },
+  --   },
+  -- }
   startify.opts.layout[6] = startify.section.mru
-
-  table.insert(startify.opts.layout, 7, {
-    type = 'padding',
-    val = 1,
-  })
-
-  startify.section.bookmarks = {
-    type = 'group',
-    val = {
-      {
-        type = 'text',
-        val = 'Bookmarks',
-        opts = { hl = 'SpecialComment' },
-      },
-      { type = 'padding', val = 1 },
-      {
-        type = 'group',
-        val = {
-          startify.file_button(
-          '~/.config/nvim/lua/user/plugins/init.lua',
-          'c'
-          ),
-          startify.file_button('~/.zshrc', 'z'),
-        },
-      },
-    },
-  }
-
-  table.insert(startify.opts.layout, 8, startify.section.bookmarks)
 
   -- update the title of the mru section and only show 5 items
   startify.section.mru.val[2].val = 'Recent'
   startify.section.mru.val[4].val = function()
     return { startify.mru(5, nil, 5) }
   end
+
+  -- Add padding to the end of the mru section
+  table.insert(startify.section.mru.val, {
+    type = 'padding',
+    val = 1,
+  })
+
 
   -- update the title of the mru_cwd section and only show 5 items
   startify.section.mru_cwd.val[2].val = 'Recent (cwd)'
