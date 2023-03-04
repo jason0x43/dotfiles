9d7ee659715dfe5c758c33c6f96e96ef12083d4d
Jason Cheatham <jason@jasoncheatham.com>
2023-03-04 19:51:57 +0000

    Improve cterm lualine theme

--- config/nvim/lua/lualine/themes/wezterm.lua
+++ config/nvim/lua/lualine/themes/wezterm.lua
@@ -4,16 +4,16 @@ local c = load_colors()
 
 return {
   normal = {
-    a = { fg = c.bg_0, bg = c.blue, gui = 'bold' },
+    a = { fg = c.blue, gui = 'bold,reverse' },
     b = { fg = c.fg_0, bg = c.bg_2 },
     c = { fg = c.fg_0, bg = c.bg_1 },
   },
 
-  insert = { a = { fg = c.bg_0, bg = c.green, gui = 'bold' } },
+  insert = { a = { fg = c.green, gui = 'bold,reverse' } },
 
-  visual = { a = { fg = c.bg_0, bg = c.violet, gui = 'bold' } },
+  visual = { a = { fg = c.violet, gui = 'bold,reverse' } },
 
-  replace = { a = { fg = c.bg_0, bg = c.red, gui = 'bold' } },
+  replace = { a = { fg = c.red, gui = 'bold,reverse' } },
 
   inactive = {
     a = { fg = c.bg_0, bg = c.bg_1 },
