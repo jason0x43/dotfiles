d497c040b97a93cc75bf2638e9638e56bfa70140
Jason Cheatham <jason@jasoncheatham.com>
2023-03-09 14:06:26 -0500

    Remove unnecessary utility functions

--- config/nvim/lua/user/util/init.lua
+++ config/nvim/lua/user/util/init.lua
@@ -1,38 +1,13 @@
 local M = {}
-local fn = vim.fn
 
 -- yank to terminal
 -- https://sunaku.github.io/tmux-yank-osc52.html
 function M.yank(text)
-  local escape = fn.system('term_copy', text)
+  local escape = vim.fn.system('term_copy', text)
   if vim.v.shell_error == 1 then
     vim.cmd('echoerr ' .. escape)
   else
-    fn.writefile({ escape }, '/dev/tty', 'b')
-  end
-end
-
--- create an augroup from a name and list of commands
--- commands are of the form
---   { group, definition }
-function M.augroup(name, commands)
-  vim.cmd('augroup ' .. name)
-  vim.cmd('autocmd!')
-  for _, def in ipairs(commands) do
-    vim.cmd('autocmd ' .. def)
-  end
-  vim.cmd('augroup END')
-end
-
--- create a vim command
-function M.cmd(name, argsOrCmd, cmd)
-  local args = cmd and argsOrCmd or nil
-  cmd = cmd and cmd or argsOrCmd
-
-  if args == nil then
-    vim.cmd('command! ' .. name .. ' ' .. cmd)
-  else
-    vim.cmd('command! ' .. args .. ' ' .. name .. ' ' .. cmd)
+    vim.fn.writefile({ escape }, '/dev/tty', 'b')
   end
 end
 
@@ -43,7 +18,7 @@ function M.text_mode()
   vim.wo.list = false
   vim.wo.signcolumn = 'no'
 
-	vim.keymap.set('', 'k', 'gk', { buffer = 0 })
+  vim.keymap.set('', 'k', 'gk', { buffer = 0 })
   vim.keymap.set('', 'j', 'gj', { buffer = 0 })
   vim.keymap.set('', '$', 'g$', { buffer = 0 })
   vim.keymap.set('', '^', 'g^', { buffer = 0 })
@@ -57,36 +32,16 @@ function M.show_view_width()
   if vim.wo.diff or filetype == 'help' or tw == 0 then
     vim.wo.colorcolumn = ''
   else
-    vim.wo.colorcolumn = fn.join(fn.range(tw + 1, tw + 1 + vim.go.columns), ',')
+    vim.wo.colorcolumn =
+      vim.fn.join(vim.fn.range(tw + 1, tw + 1 + vim.go.columns), ',')
   end
 end
 
 -- set window height within a min/max range
 function M.adjust_window_height(minheight, maxheight)
-  local line = fn.line('$')
-  local val = fn.max({ fn.min({ line, maxheight }), minheight })
+  local line = vim.fn.line('$')
+  local val = vim.fn.max({ vim.fn.min({ line, maxheight }), minheight })
   vim.cmd(val .. 'wincmd _')
-end
-
--- TS filetypes
-M.ts_types = {
-  'typescript',
-  'typescript.jsx',
-  'typescriptreact',
-  'tsx',
-  'javascript',
-  'javascript.jsx',
-  'javascriptreact',
-  'jsx',
-}
-
-local ts_types_str
-
-M.ts_types_str = function()
-  if not ts_types_str then
-    ts_types_str = table.concat(M.ts_types, ',')
-  end
-  return ts_types_str
 end
 
 -- restore the cursor position in a file
@@ -102,33 +57,36 @@ function M.restore_cursor()
     return
   end
 
-  local line = fn.line
+  local line = vim.fn.line
   if line('\'"') >= 1 and line('\'"') <= line('$') then
     vim.cmd('normal! g`"zz')
   end
 end
 
+-- print the syntax highlight information at the current location
 function M.print_syn_group()
-  local ls = fn.synID(fn.line('.'), fn.col('.'), 1)
-  if ls ~= 0 then
-    print(
-      fn.synIDattr(ls, 'name')
-        .. ' -> '
-        .. fn.synIDattr(fn.synIDtrans(ls), 'name')
-    )
+  local buf = vim.api.nvim_get_current_buf()
+  local ts_hl = require('vim.treesitter.highlighter')
+  if ts_hl.active[buf] then
+    vim.cmd('TSHighlightCapturesUnderCursor')
   else
-    print('No highlight')
+    local ls = vim.fn.synID(vim.fn.line('.'), vim.fn.col('.'), 1)
+    if ls ~= 0 then
+      print(
+        vim.fn.synIDattr(ls, 'name')
+          .. ' -> '
+          .. vim.fn.synIDattr(vim.fn.synIDtrans(ls), 'name')
+      )
+    else
+      print('No highlight')
+    end
   end
 end
 
+-- return true if the cwd is in a git work tree
 function M.in_git_dir()
   vim.fn.system({ 'git', 'rev-parse', '--is-inside-work-tree' })
   return vim.v.shell_error == 0
-end
-
-function M.get_script_path()
-  local str = debug.getinfo(2, 'S').source:sub(2)
-  return str:match('(.*/)')
 end
 
 return M
