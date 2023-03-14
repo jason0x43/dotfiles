ddafcc219ba011bd15b2468fa6f52c0fbcb6c723
Jason Cheatham <jason@jasoncheatham.com>
2023-03-14 12:29:24 -0400

    Aerial setup
    - Close when selecting a symbol
    - Focus aerial when opening
    - Subtler highlighting

--- config/nvim/lua/user/plugins/aerial.lua
+++ config/nvim/lua/user/plugins/aerial.lua
@@ -2,13 +2,19 @@ return {
   'stevearc/aerial.nvim',
 
   config = function()
-    require('aerial').setup({
+    local aerial = require('aerial')
+    aerial.setup({
+      close_on_select = true,
       layout = {
-				max_width = 0.3,
+        max_width = 0.3,
         width = 30,
       },
     })
 
-    vim.keymap.set('n', '<leader>s', '<cmd>AerialToggle!<cr>')
+    vim.keymap.set('n', '<leader>s', function()
+      aerial.toggle({ focus = true })
+    end)
+
+    vim.api.nvim_set_hl(0, 'AerialLine', { link = 'CursorLine' })
   end,
 }
