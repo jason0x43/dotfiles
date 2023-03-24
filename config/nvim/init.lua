6aa2bf6bcb5cd648f4651f2204a8aee4d915f76b
Jason Cheatham <jason@jasoncheatham.com>
2023-03-24 14:25:50 -0400

    Remove user.config.ui (custom float)
    Should already be handled by other plugins

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,6 +2,5 @@ require('user.config.options')
 require('user.config.lazy')
 require('user.config.keymaps')
 require('user.config.autocommands')
-require('user.config.ui')
 
 vim.api.nvim_command('colorscheme wezterm')
