35e8e9d5e31edb9d0815634d6f0c315601a76e55
Jason Cheatham <jason@jasoncheatham.com>
2023-04-02 11:23:40 -0400

    Compute diffs as-needed in undotree-nvim

--- config/nvim/lua/undotree-nvim/init.lua
+++ config/nvim/lua/undotree-nvim/init.lua
@@ -10,78 +10,15 @@ local context_lines = 3
 local function _build_tree(entries, level)
   local undolist = {}
 
-  -- create diffs for each entry in undotree
   for i = #entries, 1, -1 do
-    -- grab the buffer as it is after this iteration's undo state
-    vim.cmd('silent undo ' .. entries[i].seq)
-    local buffer_after_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false) or {}
-    local buffer_after = table.concat(buffer_after_lines, '\n')
-
-    -- grab the buffer as it is after this undo state's parent
-    vim.cmd('silent undo')
-    local buffer_before_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-      or {}
-    local buffer_before = table.concat(buffer_before_lines, '\n')
-
-    -- add a diff file header for this hunk for delta
-    local filename = vim.fn.expand('%')
-    local diff = '--- ' .. filename .. '\n+++ ' .. filename .. '\n'
-
-    local on_hunk_callback = function(start_a, count_a, start_b, count_b)
-      -- add a diff location header for this hunk for delta
-      diff = diff .. '@@ -' .. start_a
-      if count_a ~= 1 then
-        diff = diff .. ',' .. count_a
-      end
-      diff = diff .. ' +' .. start_b
-      if count_b ~= 1 then
-        diff = diff .. ',' .. count_b
-      end
-      diff = diff .. ' @@'
-
-      -- add context before diff
-      for x = start_a - context_lines, start_a - 1 do
-        if buffer_before_lines[x] ~= nil then
-          diff = diff .. '\n ' .. buffer_before_lines[x]
-        end
-      end
-
-      -- add deletions
-      for j = start_a, start_a + count_a - 1 do
-        diff = diff .. '\n-' .. buffer_before_lines[j]
-      end
-
-      -- add additions
-      for k = start_b, start_b + count_b - 1 do
-        diff = diff .. '\n+' .. buffer_after_lines[k]
-      end
-
-      -- add context after diff
-      for n = start_a + count_a, start_a + count_a + context_lines - 1 do
-        if buffer_before_lines[n] ~= nil then
-          diff = diff .. '\n ' .. buffer_before_lines[n]
-        end
-      end
-
-      -- end with a newline
-      diff = diff .. '\n'
-    end
-
-    vim.diff(buffer_before, buffer_after, {
-      result_type = 'indices',
-      on_hunk = on_hunk_callback,
-      algorithm = 'patience',
-    })
-
     table.insert(undolist, {
       seq = entries[i].seq,
       alt = level,
       first = i == #entries,
       time = entries[i].time,
-      diff = diff,
     })
 
-    -- if the entry has an alt branch, generate undo items for that and add it
+    -- if the entry has an alt branch, generate undo items for that and add them
     -- to the undolist
     if entries[i].alt ~= nil then
       local alt_undolist = _build_tree(entries[i].alt, level + 1)
@@ -95,17 +32,85 @@ local function _build_tree(entries, level)
 end
 
 local function build_tree(ut)
-  -- save the cursor position -- it will need to be restored after building the
-  -- tree, which changes the buffer while creating diffs
-  local cursor = vim.api.nvim_win_get_cursor(0)
-
-  local undolist = _build_tree(ut.entries, 0)
-
-  -- restore the buffer and cursor after building the tree
-  vim.cmd('silent undo ' .. ut.seq_cur)
-  vim.api.nvim_win_set_cursor(0, cursor)
+  local ok, undolist = pcall(_build_tree, ut.entries, 0)
+  if not ok then
+    vim.notify('Error building undo list\n' .. undolist, vim.log.levels.ERROR)
+    undolist = nil
+  end
 
   return undolist
+end
+
+local function get_diff(seq, seq_cur)
+  -- save the cursor position -- it will need to be restored after building the
+  -- tree, which changes the buffer while creating diffs
+  local savedview = vim.fn.winsaveview()
+
+  -- grab the buffer as it is after this iteration's undo state
+  vim.cmd('silent undo ' .. seq)
+  local buffer_after_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false) or {}
+  local buffer_after = table.concat(buffer_after_lines, '\n')
+
+  -- grab the buffer as it is after this undo state's parent
+  vim.cmd('silent undo')
+  local buffer_before_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false) or {}
+  local buffer_before = table.concat(buffer_before_lines, '\n')
+
+  -- -- add a diff file header for this hunk for delta
+  local filename = vim.fn.expand('%')
+  local diff = '--- ' .. filename .. '\n+++ ' .. filename .. '\n'
+
+  local on_hunk_callback = function(start_a, count_a, start_b, count_b)
+    -- add a diff location header for this hunk for delta
+    diff = diff .. '@@ -' .. start_a
+    if count_a ~= 1 then
+      diff = diff .. ',' .. count_a
+    end
+    diff = diff .. ' +' .. start_b
+    if count_b ~= 1 then
+      diff = diff .. ',' .. count_b
+    end
+    diff = diff .. ' @@'
+
+    -- add context before diff
+    for x = start_a - context_lines, start_a - 1 do
+      if buffer_before_lines[x] ~= nil then
+        diff = diff .. '\n ' .. buffer_before_lines[x]
+      end
+    end
+
+    -- add deletions
+    for j = start_a, start_a + count_a - 1 do
+      diff = diff .. '\n-' .. buffer_before_lines[j]
+    end
+
+    -- add additions
+    for k = start_b, start_b + count_b - 1 do
+      diff = diff .. '\n+' .. buffer_after_lines[k]
+    end
+
+    -- add context after diff
+    for n = start_a + count_a, start_a + count_a + context_lines - 1 do
+      if buffer_before_lines[n] ~= nil then
+        diff = diff .. '\n ' .. buffer_before_lines[n]
+      end
+    end
+
+    -- end with a newline
+    diff = diff .. '\n'
+  end
+
+  vim.diff(buffer_before, buffer_after, {
+    result_type = 'indices',
+    on_hunk = on_hunk_callback,
+    algorithm = 'patience',
+  })
+
+  -- restore the buffer and cursor after building the tree
+  vim.cmd('silent undo ' .. seq_cur)
+  vim.fn.winrestview(savedview)
+
+  return diff
 end
 
 local function time_value(value, unit)
@@ -192,7 +197,20 @@ local function render_undo_entry(undo, seq_cur)
   return line
 end
 
+local function highlight_diff(diff)
+  return vim.fn.system(
+    'delta --color-only'
+      .. ' --minus-style 1'
+      .. ' --minus-emph-style "1 reverse"'
+      .. ' --plus-style 2'
+      .. ' --plus-emph-style "2 reverse"',
+    diff
+  )
+end
+
 M.undotree = function()
+  local buf = vim.api.nvim_get_current_buf()
+
   local ut = vim.fn.undotree()
   if #ut.entries <= 1 then
     vim.notify('No undo history', vim.log.levels.WARN)
@@ -200,6 +218,10 @@ M.undotree = function()
   end
 
   local tree = build_tree(ut)
+  if tree == nil then
+    return
+  end
+
   local lines = {}
   local entries = {}
 
@@ -225,8 +247,18 @@ M.undotree = function()
         local contents = {}
         vim.tbl_map(function(item)
           local seq = item:match('^(%d+)')
-          local diff = entries[seq].diff
-          local hldiff = vim.fn.system('delta --diff-highlight', diff)
+
+          local ok, diff = pcall(vim.api.nvim_buf_call, buf, function()
+            return get_diff(seq, ut.seq_cur)
+          end)
+
+          local hldiff
+          if ok then
+            hldiff = highlight_diff(diff)
+          else
+            hldiff = diff
+          end
+
           table.insert(contents, hldiff)
         end, items)
         return contents
