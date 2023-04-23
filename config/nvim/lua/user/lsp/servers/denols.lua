8bfcbd7ae99030f20fb7a85c3cefc455087f10d2
Jason Cheatham <jason@jasoncheatham.com>
2023-04-22 21:06:46 -0400

    Cleanup

--- config/nvim/lua/user/lsp/servers/denols.lua
+++ config/nvim/lua/user/lsp/servers/denols.lua
@@ -3,14 +3,12 @@ local lspconfig = require('lspconfig')
 
 return {
   root_dir = lspconfig.util.root_pattern('deno.json', 'deno.jsonc'),
-
   init_options = {
     lint = true,
     unstable = true,
   },
-
   handlers = {
-    ['textDocument/definition'] = function(err, result, ctx, config)
+    ['textDocument/definition'] = function(err, result, ctx)
       -- If denols returns multiple results, goto the first local one
       if result and #result > 1 then
         result = { result[1] }
@@ -18,11 +16,9 @@ return {
       denols.default_config.handlers['textDocument/definition'](
         err,
         result,
-        ctx,
-        config
+        ctx
       )
     end,
-
     ['textDocument/codeAction'] = function(err, result, ctx, config)
       print('handling deno codeaction')
       denols.default_config.handlers['textDocument/codeAction'](
