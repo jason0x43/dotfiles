830194d23fea0935998cb21190d1ed8c044e27ba
Jason Cheatham <jason@jasoncheatham.com>
2023-04-24 15:40:18 -0400

    Disable colorizer for large files

--- config/nvim/lua/user/plugins/ui.lua
+++ config/nvim/lua/user/plugins/ui.lua
@@ -4,9 +4,20 @@ return {
     'norcalli/nvim-colorizer.lua',
     cond = vim.go.termguicolors,
     config = function()
-      require('colorizer').setup({ '*' }, {
+      require('colorizer').setup({}, {
         names = false,
         rgb_fn = true,
+      })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = '*',
+        callback = function(ev)
+          local bufnr = ev.buf
+          if require('user.util').is_large_file(bufnr) then
+            return
+          end
+          require('colorizer').attach_to_buffer(bufnr)
+        end
       })
     end,
   },
