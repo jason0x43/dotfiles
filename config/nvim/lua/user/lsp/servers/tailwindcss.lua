50d069380854b9400cec85db8e95eb5c2604eb39
Jason Cheatham <jason@jasoncheatham.com>
2024-02-25 20:31:47 -0700

    Load Tailwind LS for TS config files

--- config/nvim/lua/user/lsp/servers/tailwindcss.lua
+++ config/nvim/lua/user/lsp/servers/tailwindcss.lua
@@ -2,7 +2,8 @@ return {
   root_dir = function(fname)
     return require('lspconfig').util.root_pattern(
       'tailwind.config.cjs',
-      'tailwind.config.js'
+      'tailwind.config.js',
+      'tailwind.config.ts'
     )(fname)
   end,
 }
