40d4b3332513fd5d9a19e32f23c9ac50590ffcff
Jason Cheatham <jason@jasoncheatham.com>
2024-04-20 16:16:17 -0600

    Add some shortcuts for opening Trouble v3 displays

--- config/nvim/lua/user/plugins/lsp.lua
+++ config/nvim/lua/user/plugins/lsp.lua
@@ -67,7 +67,41 @@ return {
     branch = 'dev',
     event = 'BufEnter',
     dependencies = 'nvim-tree/nvim-web-devicons',
-    config = true,
+    keys = {
+      {
+        '<leader>td',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>tD',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>ts',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>tl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>tL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>tQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
+    opts = {
+      focus = true,
+    },
   },
 
   -- virtual text diagnostics
