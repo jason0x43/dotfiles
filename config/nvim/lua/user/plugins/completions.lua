1c0dd3eb0eb7d1c9a55eecda91f70cbcd882aceb
Jason Cheatham <jason@jasoncheatham.com>
2024-04-22 13:45:22 -0600

    Remove luasnip in favor of built in snippets

--- config/nvim/lua/user/plugins/completions.lua
+++ config/nvim/lua/user/plugins/completions.lua
@@ -3,52 +3,27 @@ return {
     'hrsh7th/nvim-cmp',
 
     dependencies = {
-      'L3MON4D3/LuaSnip',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lua',
       'hrsh7th/cmp-nvim-lsp',
-      'saadparwaiz1/cmp_luasnip',
     },
 
     opts = function()
       vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
       local cmp = require('cmp')
-      local luasnip = require('luasnip')
 
       return {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
         mapping = {
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          -- ['<C-e>'] = cmp.mapping.abort(),
-          -- ['<C-p>'] = cmp.mapping.select_prev_item(),
-          -- ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-n>'] = cmp.mapping.select_next_item(),
           ['<C-e>'] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Insert,
             select = true,
           }),
-          ['<C-n>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip and luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<C-p>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
         },
         sources = {
           { name = 'copilot' },
