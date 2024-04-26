edfb5a9b48bd980006e21afcbda14466dec4944e
Jason Cheatham <jason@jasoncheatham.com>
2024-04-25 23:53:26 -0600

    Cleanup

--- config/nvim/lua/user/plugins/ui.lua
+++ config/nvim/lua/user/plugins/ui.lua
@@ -32,7 +32,7 @@ return {
   },
 
   {
-    "JManch/sunset.nvim",
+    'JManch/sunset.nvim',
     lazy = false,
     enabled = function()
       local locfile = vim.fn.expand('~/.config/location.json')
@@ -41,15 +41,14 @@ return {
     opts = function()
       local locfile = vim.fn.expand('~/.config/location.json')
       if vim.fn.filereadable(locfile) == 0 then
-        print('not readable')
         return
       end
       local location = vim.fn.json_decode(vim.fn.join(vim.fn.readfile(locfile)))
 
       return {
-        latitude = location["latitude"],
-        longitude = location["longitude"]
+        latitude = location['latitude'],
+        longitude = location['longitude'],
       }
-    end
-  }
+    end,
+  },
 }
