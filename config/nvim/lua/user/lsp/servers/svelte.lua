539bda3ca39cbc58af1151e7a7a0aad9a350b413
Jason Cheatham <jason@jasoncheatham.com>
2024-05-18 15:29:35 -0600

    Update svelte ls config

--- config/nvim/lua/user/lsp/servers/svelte.lua
+++ config/nvim/lua/user/lsp/servers/svelte.lua
@@ -1,14 +1,9 @@
 return {
   capabilities = {
     workspace = {
-      -- Completely disable didChangeWatchedFiles; svelte won't start its
-      -- fallback file watcher if it sees a non-falsy value for
-      -- didChangeWatchedFiles.
-      -- svelte-language-server doesn't register file watchers, and it looks
-      -- like nvim's implementation is only sending events to registered
-			-- watchers (which is the recommendation in the spec
-      -- https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#workspace_didChangeWatchedFiles)
-      didChangeWatchedFiles = false,
+      didChangeWatchedFiles = {
+        dynamicRegistration = true,
+      },
     },
   },
 }
