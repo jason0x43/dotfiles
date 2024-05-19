2e05531e3d4c8c22cdef231231960c05eccf7184
Jason Cheatham <jason@jasoncheatham.com>
2024-05-18 21:10:01 -0600

    Add a Blink theme

---
+++ terminal/Selenized_black.js
@@ -0,0 +1,21 @@
+t.prefs_.set("color-palette-overrides", [
+	"#181818",
+	"#ed4a46",
+	"#70b433",
+	"#dbb32d",
+	"#368aeb",
+	"#eb6eb7",
+	"#3fc5b7",
+	"#777777",
+	"#252525",
+	"#ff5e56",
+	"#83c746",
+	"#efc541",
+	"#4f9cfe",
+	"#ff81ca",
+	"#56d8c9",
+	"#b9b9b9",
+]);
+t.prefs_.set("foreground-color", "#b9b9b9");
+t.prefs_.set("background-color", "#181818");
+t.prefs_.set("cursor-color", "rgba(255,255,255,0.5)");
