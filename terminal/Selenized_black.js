f8ea66347be229c79762b4b7f943af129ce20966
Jason Cheatham <jason@jasoncheatham.com>
2024-05-18 21:12:30 -0600

    Fix colors in the blink theme

--- terminal/Selenized_black.js
+++ terminal/Selenized_black.js
@@ -1,5 +1,5 @@
 t.prefs_.set("color-palette-overrides", [
-	"#181818",
+	"#252525",
 	"#ed4a46",
 	"#70b433",
 	"#dbb32d",
@@ -7,14 +7,14 @@ t.prefs_.set("color-palette-overrides", [
 	"#eb6eb7",
 	"#3fc5b7",
 	"#777777",
-	"#252525",
+	"#505050",
 	"#ff5e56",
 	"#83c746",
 	"#efc541",
 	"#4f9cfe",
 	"#ff81ca",
 	"#56d8c9",
-	"#b9b9b9",
+	"#dedede",
 ]);
 t.prefs_.set("foreground-color", "#b9b9b9");
 t.prefs_.set("background-color", "#181818");
