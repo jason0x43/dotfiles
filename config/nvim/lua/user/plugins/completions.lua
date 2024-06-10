e9b580776a791c1edb8b871dcc61a23ee1bd2c9b
Jason Cheatham <jason@jasoncheatham.com>
2024-06-10 08:50:20 -0600

    Try to fix some completion annoyances
    Don't let completions overwrite current text, breaking later
    completions

--- config/nvim/lua/user/plugins/completions.lua
+++ config/nvim/lua/user/plugins/completions.lua
@@ -14,12 +14,35 @@ return {
 
       local cmp = require('cmp')
 
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0
+          and vim.api
+              .nvim_buf_get_lines(0, line - 1, line, true)[1]
+              :sub(col, col)
+              :match('%s')
+            == nil
+      end
+
       return {
         mapping = {
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-n>'] = function(fallback)
+            if cmp.visible() and has_words_before() then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              fallback()
+            end
+          end,
+          ['<C-p>'] = function(fallback)
+            if cmp.visible() and has_words_before() then
+              cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+            else
+              fallback()
+            end
+          end,
           ['<C-e>'] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Insert,
             select = true,
@@ -27,10 +50,9 @@ return {
         },
         sources = {
           { name = 'copilot' },
-          { name = 'lazydev', group_index = 0 },
+          { name = 'lazydev' },
           { name = 'nvim_lsp' },
           { name = 'path' },
-          { name = 'luasnip' },
           { name = 'buffer' },
         },
         preselect = 'none',
