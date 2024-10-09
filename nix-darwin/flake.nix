1b01d66aeca3a6aa1d0be4b332a5de63d8253209
Jason Cheatham <jason@jasoncheatham.com>
2024-10-08 23:48:37 -0600

    Add nix flake

---
+++ nix-darwin/flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [
+            pkgs.neovim
+        ];
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Add main user to trusted list.
+      nix.settings.trusted-users = [ "root" "jason" ];
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true; # default shell on catalina
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      # Add ability to used TouchID for sudo authentication
+      security.pam.enableSudoTouchIdAuth = true;
+
+      # Configure macOS system options
+      system.defaults = {
+        dock.autohide = true;
+        dock.mru-spaces = false;
+        finder.AppleShowAllExtensions = true;
+        finder.FXPreferredViewStyle = "clmv";
+        screencapture.location = "~/Downloads";
+        screensaver.askForPasswordDelay = 10;
+      };
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."Jasons-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."Jasons-MacBook-Pro".pkgs;
+  };
+}
