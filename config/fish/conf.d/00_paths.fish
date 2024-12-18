6514650926fa7734abed15097a82939cafabb5b5
Jason Cheatham <jason@jasoncheatham.com>
2024-12-18 13:19:23 -0700

    Add usr/local/bin and deno/bin to path

--- config/fish/conf.d/00_paths.fish
+++ config/fish/conf.d/00_paths.fish
@@ -1,5 +1,7 @@
 fish_add_path -m /opt/homebrew/bin
 fish_add_path -m /home/linuxbrew/.linuxbrew/bin
+fish_add_path -m /usr/local/bin
+fish_add_path -m ~/.dotfiles/bin
 fish_add_path -m ~/.orbstack/bin
 fish_add_path -m ~/.cargo/bin
-fish_add_path -m ~/.dotfiles/bin
+fish_add_path -m ~/.deno/bin
