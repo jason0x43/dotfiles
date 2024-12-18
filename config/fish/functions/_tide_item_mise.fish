43b5fe9de028647143361666362005867cc2db91
Jason Cheatham <jason@jasoncheatham.com>
2024-12-18 13:20:28 -0700

    Handle scoped tool versions in fish mise component

--- config/fish/functions/_tide_item_mise.fish
+++ config/fish/functions/_tide_item_mise.fish
@@ -8,6 +8,12 @@ function _tide_item_mise
         set -l parts (string split ' ' $plugin)
         set -l tool $parts[1]
         set -l ver $parts[2]
+
+        if string match -q "*@*" $ver
+            set -l subparts (string split '@' $ver)
+            set ver $subparts[2]
+        end
+
         set -l icon_var tide_"$tool"_icon
         _tide_print_item $tool $$icon_var' ' $ver
     end
