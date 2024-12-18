ccab6da38dc108036471780deb0c020df4d3630f
Jason Cheatham <jason@jasoncheatham.com>
2024-12-18 13:19:36 -0700

    Globally disable corepack's annoying package.json updates

--- config/fish/conf.d/01_env.fish
+++ config/fish/conf.d/01_env.fish
@@ -2,3 +2,4 @@ set -gx SHELL (which fish)
 set -gx NODE_OPTIONS --disable-warning=ExperimentalWarning
 set -gx EDITOR nvim
 set -gx RIPGREP_CONFIG_PATH ~/.config/ripgrep
+set -gx COREPACK_ENABLE_AUTO_PIN 0
