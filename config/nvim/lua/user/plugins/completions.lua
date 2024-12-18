c157efa9c38a49aebd8ea06c86745b0f927ab86d
Jason Cheatham <jason@jasoncheatham.com>
2024-12-18 13:39:34 -0700

    Use proper blink config format

--- config/nvim/lua/user/plugins/completions.lua
+++ config/nvim/lua/user/plugins/completions.lua
@@ -1,16 +1,35 @@
 return {
   {
     'saghen/blink.cmp',
-    version = 'v0.7.6',
+    version = 'v0.*',
     opts = {
       sources = {
-        default = { 'lsp', 'copilot', 'path', 'buffer', 'lazydev' },
+        -- TODO: use this on next version update
+        -- default = { 'lsp', 'copilot', 'path', 'buffer', 'lazydev' },
+        completion = {
+          enabled_providers = { 'lsp', 'copilot', 'path', 'buffer', 'lazydev' },
+        },
+        providers = {
+          copilot = {
+            name = 'copilot',
+            module = 'blink-cmp-copilot',
+            score_offset = 100,
+            async = true,
+          },
+          lsp = {
+            fallback_for = { 'lazydev' },
+          },
+          lazydev = {
+            name = 'LazyDev',
+            module = 'lazydev.integrations.blink',
+          },
+        },
       },
       signature = { enabled = true },
       keymap = {
         preset = 'default',
         ['<C-e>'] = { 'select_and_accept' },
-      }
+      },
     },
   },
 
