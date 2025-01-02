d39dc284df72da7d7f5fff65511b4f129596aa9a
Jason Cheatham <jason@jasoncheatham.com>
2025-01-02 11:19:26 -0700

    Disable blink cmdline completions

--- config/nvim/lua/user/plugins/completions.lua
+++ config/nvim/lua/user/plugins/completions.lua
@@ -4,8 +4,8 @@ return {
     version = 'v0.*',
     opts = {
       sources = {
-        -- TODO: use this on next version update
         default = { 'lsp', 'copilot', 'path', 'buffer', 'lazydev' },
+        cmdline = {},
         providers = {
           copilot = {
             name = 'copilot',
@@ -24,11 +24,6 @@ return {
       keymap = {
         preset = 'default',
         ['<C-e>'] = { 'select_and_accept' },
-        -- cmdline = {
-        --   preset = 'enter',
-        --   -- ['<Tab>'] = { 'select_next', 'fallback' },
-        --   -- ['<S-Tab>'] = { 'select_prev', 'fallback' },
-        -- },
       },
     },
   },
