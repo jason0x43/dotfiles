066faad83a4704748b632ea08c81f74da4fc4491
Jason Cheatham <jason@jasoncheatham.com>
2025-02-14 10:01:39 -0700

    Tweak MiniGit blame display

--- config/nvim/lua/user/config/lazy.lua
+++ config/nvim/lua/user/config/lazy.lua
@@ -63,6 +63,25 @@ require('lazy').setup(
         -- Git integration; used by statusline
         require('mini.git').setup()
 
+        vim.api.nvim_create_autocmd('User', {
+          pattern = 'MiniGitCommandSplit',
+          callback = function(event)
+            if event.data.git_subcommand ~= 'blame' then
+              return
+            end
+
+            -- Align blame output with source
+            print('aligning blame with source')
+            local win_src = event.data.win_source
+            vim.wo.wrap = false
+            vim.fn.winrestview({ topline = vim.fn.line('w0', win_src) })
+            vim.api.nvim_win_set_cursor(0, { vim.fn.line('.', win_src), 0 })
+
+            -- Bind both windows so that they scroll together
+            vim.wo[win_src].scrollbind, vim.wo.scrollbind = true, true
+          end,
+        })
+
         -- Diffing; used by status line
         require('mini.diff').setup()
         vim.api.nvim_create_user_command('Diff', function()
