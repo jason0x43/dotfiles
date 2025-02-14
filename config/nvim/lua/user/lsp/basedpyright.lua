f1f516a1a1d5d019fef5bc34f933809f33a62ac6
Jason Cheatham <jason@jasoncheatham.com>
2025-02-14 09:25:50 -0700

    Inform neovim of basedpyright.createtypstub command

--- config/nvim/lua/user/lsp/basedpyright.lua
+++ config/nvim/lua/user/lsp/basedpyright.lua
@@ -1,5 +1,16 @@
 local C = require('user.util.lsp').make_user_config()
 
+C.config.on_attach = function(client)
+  -- Let neovim know about custom basedpyright commands that may be used by code
+  -- actions
+  local provider = client.server_capabilities.executeCommandProvider
+  if provider then
+    provider.commands = vim.tbl_extend('keep', provider.commands, {
+      'basedpyright.createtypestub',
+    })
+  end
+end
+
 C.config.handlers = {
   ['textDocument/publishDiagnostics'] = function(err, result, ctx, config)
     result.diagnostics = vim.tbl_filter(function(diag)
