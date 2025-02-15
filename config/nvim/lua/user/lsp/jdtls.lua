ed2a0561d36f11723550a277c10f7503ccd040eb
Jason Cheatham <jason@jasoncheatham.com>
2025-02-14 22:49:20 -0700

    Go back to the mostly the default jdtls config

--- config/nvim/lua/user/lsp/jdtls.lua
+++ config/nvim/lua/user/lsp/jdtls.lua
@@ -1,45 +1,13 @@
 local C = require('user.util.lsp').make_user_config()
 
----Add any jdtls-specific JVM args from the environment
-local function get_jdtls_jvm_args()
-  local args = {}
-  for a in string.gmatch((os.getenv('JDTLS_JVM_ARGS') or ''), '%S+') do
-    local arg = string.format('--jvm-arg=%s', a)
-    table.insert(args, arg)
-  end
-  return unpack(args)
-end
-
 -- Update the server config when a new root directory is detected
-C.config.on_new_config = function(new_config, new_root_dir)
-  local workspace_dir = new_root_dir .. '/.jdtls/workspace'
-
-  new_config.cmd = {
-    'jdtls',
-    '-configuration',
-    new_root_dir .. '/.jdtls/config',
-    '-data',
-    workspace_dir,
-    get_jdtls_jvm_args(),
-  }
-
-  new_config.init_options = {
-    workspace = workspace_dir,
-  }
-
-  new_config.settings = {
-    java = {
-      project = {
-        sourcePaths = {
-          new_root_dir,
-        },
-      },
-      autobuild = {
-        enabled = true
-      }
+C.config.settings = {
+  java = {
+    symbols = {
+      includeSourceMethodDeclarations = true,
     },
-  }
-end
+  },
+}
 
 C.config.handlers = {
   ['textDocument/publishDiagnostics'] = function(err, result, ctx, config)
