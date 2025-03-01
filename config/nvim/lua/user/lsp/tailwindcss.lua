f121409ce22c309c4f245e4bcd450ff22c2136ff
Jason Cheatham <jason@jasoncheatham.com>
2025-03-01 11:04:34 -0700

    Use ts file for tailwind root detection

--- config/nvim/lua/user/lsp/tailwindcss.lua
+++ config/nvim/lua/user/lsp/tailwindcss.lua
@@ -3,7 +3,12 @@ local C = require('user.util.lsp').make_user_config()
 C.should_start = function(file)
   local tailwindCfg = vim.fs.root(
     file,
-    { 'tailwind.config.js', 'tailwind.config.cjs', 'tailwind.config.mjs' }
+    {
+      'tailwind.config.ts',
+      'tailwind.config.js',
+      'tailwind.config.cjs',
+      'tailwind.config.mjs',
+    }
   )
   return tailwindCfg ~= nil
 end
