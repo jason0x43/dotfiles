b3b6c991db1aa4d37d9966426187b329db55f46e
Jason Cheatham <jason@jasoncheatham.com>
2025-03-14 08:00:40 -0600

    Don't use fish user path

--- config/fish/conf.d/00_paths.fish
+++
@@ -1,8 +0,0 @@
-fish_add_path -m /opt/homebrew/bin
-fish_add_path -m /opt/homebrew/opt/ruby/bin
-fish_add_path -m /home/linuxbrew/.linuxbrew/bin
-fish_add_path -m /usr/local/bin
-fish_add_path -m ~/.dotfiles/bin
-fish_add_path -m ~/.orbstack/bin
-fish_add_path -m ~/.cargo/bin
-fish_add_path -m ~/.deno/bin
---
+++ config/fish/conf.d/05_paths.fish
@@ -0,0 +1,14 @@
+set -l homebrew /opt/homebrew
+
+if test -d /home/linuxbrew
+    set homebrew /home/linuxbrew/.linuxbrew
+end
+
+fish_add_path --path $homebrew/sbin
+fish_add_path --path $homebrew/bin
+fish_add_path --path $homebrew/opt/ruby/bin
+fish_add_path --path $homebrew/opt/php@8.1/bin
+fish_add_path --path /usr/local/bin
+fish_add_path --path ~/.cargo/bin
+fish_add_path --path ~/.deno/bin
+fish_add_path --path ~/.dotfiles/bin
