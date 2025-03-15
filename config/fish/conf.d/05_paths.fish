88fcae3166577464dddf61f725b033a3ce0cd3c1
Jason Cheatham <jason@jasoncheatham.com>
2025-03-15 13:23:42 -0600

    Make node22 system default

--- config/fish/conf.d/05_paths.fish
+++ config/fish/conf.d/05_paths.fish
@@ -8,6 +8,7 @@ fish_add_path --path $homebrew/sbin
 fish_add_path --path $homebrew/bin
 fish_add_path --path $homebrew/opt/ruby/bin
 fish_add_path --path $homebrew/opt/php@8.1/bin
+fish_add_path --path $homebrew/opt/node@22/bin
 fish_add_path --path /usr/local/bin
 fish_add_path --path ~/.cargo/bin
 fish_add_path --path ~/.deno/bin
