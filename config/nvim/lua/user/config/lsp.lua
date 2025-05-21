c1b8d125cf50cae1fd2b4825b246ac66214aa686
Jason Cheatham <jason@jasoncheatham.com>
2025-05-20 21:09:26 -0600

    Improve formatting in lsp popups

--- config/nvim/lua/user/config/lsp.lua
+++ config/nvim/lua/user/config/lsp.lua
@@ -14,7 +14,12 @@ local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
   opts = opts or {}
   opts.border = opts.border or 'rounded'
-  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+  opts.max_width = vim.api.nvim_win_get_width(0) - 10
+
+  local bufnr, winid = orig_util_open_floating_preview(contents, syntax, opts, ...)
+  vim.api.nvim_set_option_value('linebreak', true, { win = winid })
+
+  return bufnr, winid
 end
 
 -- When an lsp returns multiple "goto definition" results, only keep the
