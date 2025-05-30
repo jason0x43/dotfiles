b8110346b37f2047500a3a22b9947843467c8f99
Jason Cheatham <jason@jasoncheatham.com>
2025-05-30 14:26:28 +0000

    Updates for lightsail VMs

--- config/fish/conf.d/gh-copilot.fish
+++ config/fish/conf.d/gh-copilot.fish
@@ -1,1 +1,3 @@
-source ~/.local/share/gh/extensions/gh-fish/gh-copilot-alias.fish
+if test -x ~/.local/share/gh/extensions/gh-fish/gh-copilot-alias.fish
+    source ~/.local/share/gh/extensions/gh-fish/gh-copilot-alias.fish
+end
