39d0f3cd4ccb34742ad7b3202fc91cb8427390dc
Jason Cheatham <jason@jasoncheatham.com>
2025-08-04 13:34:00 -0600

    Add large file support for nvim

--- config/nvim/lua/user/config/filetypes.lua
+++ config/nvim/lua/user/config/filetypes.lua
@@ -38,3 +38,62 @@ vim.filetype.add({
     },
   },
 })
+
+-- Based on Snacks bigfile
+vim.filetype.add({
+  pattern = {
+    ['.*'] = {
+      function(path, buf)
+        local big_size = 2 * 1024 * 1024
+        local big_line = 300
+
+        if not path or not buf or vim.bo[buf].filetype == 'bigfile' then
+          return
+        end
+
+        if path ~= vim.api.nvim_buf_get_name(buf) then
+          return
+        end
+
+        local size = vim.fn.getfsize(path)
+
+        if size <= 0 then
+          return
+        end
+
+        if size > big_size then
+          return 'bigfile'
+        end
+
+        local avg_line = size / vim.api.nvim_buf_line_count(buf)
+        if avg_line > big_line then
+          return 'bigfile'
+        end
+      end,
+    },
+  },
+})
+
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  group = vim.api.nvim_create_augroup('bigfile', { clear = true }),
+  pattern = 'bigfile',
+  callback = function(ev)
+    local buf = ev.buf
+    local ft = vim.filetype.match({ buf = buf }) or ''
+
+    vim.api.nvim_buf_call(buf, function()
+      if vim.fn.exists(':NoMatchParen') ~= 0 then
+        vim.cmd([[NoMatchParen]])
+      end
+      vim.wo.foldmethod = 'manual'
+      vim.wo.statuscolumn = ''
+      vim.wo.conceallevel = 0
+      vim.b.minianimate_disable = true
+      vim.schedule(function()
+        if vim.api.nvim_buf_is_valid(ev.buf) then
+          vim.bo[buf].syntax = ft
+        end
+      end)
+    end)
+  end,
+})
