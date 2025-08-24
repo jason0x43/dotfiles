0f72cae0a171311c4431d1f1341503beb2dc05b7
Jason Cheatham <jason@jasoncheatham.com>
2025-08-23 20:03:28 -0600

    Fix kitty-scrollback config for mini.deps

--- config/nvim/kitty-scrollback.lua
+++ config/nvim/kitty-scrollback.lua
@@ -1,3 +1,12 @@
+-- in your Neovim config
+vim.api.nvim_create_autocmd('TermOpen', {
+  pattern = '*',
+  callback = function()
+    print('opened!')
+    vim.wo.wrap = true
+  end,
+})
+
 vim.g.mapleader = ';'
 
 -- Add kitty-scrollback.nvim to the runtimepath to allow us to require the
@@ -5,8 +14,8 @@ vim.g.mapleader = ';'
 -- package manager, if you are not sure leave them all it will not cause any
 -- issues.
 vim.opt.runtimepath:append(
-  vim.fn.stdpath('data') .. '/lazy/kitty-scrollback.nvim'
-) -- lazy.nvim
+  vim.fn.stdpath('data') .. '/site/pack/deps/opt/kitty-scrollback.nvim'
+)
 
 vim.keymap.set({ 'v' }, 'Y', '<Plug>(KsbVisualYankLine)', {})
 vim.keymap.set({ 'v' }, 'y', '<Plug>(KsbVisualYank)', {})
@@ -15,5 +24,5 @@ vim.keymap.set({ 'n' }, 'y', '<Plug>(KsbNormalYank)', {})
 vim.keymap.set({ 'n' }, 'yy', '<Plug>(KsbYankLine)', {})
 
 require('kitty-scrollback').setup({
-  keymaps_enabled = false
+  keymaps_enabled = false,
 })
