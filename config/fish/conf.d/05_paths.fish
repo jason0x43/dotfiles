bb33c556ab5e8bcfab8d95ba6e83b9205e8f9a51
Jason Cheatham <jason@jasoncheatham.com>
2025-08-29 14:30:56 -0600

    Ensure fish path is in intended order

--- config/fish/conf.d/05_paths.fish
+++ config/fish/conf.d/05_paths.fish
@@ -4,15 +4,15 @@ if test -d /home/linuxbrew
     set homebrew /home/linuxbrew/.linuxbrew
 end
 
-fish_add_path --path $homebrew/sbin
-fish_add_path --path $homebrew/bin
-fish_add_path --path $homebrew/opt/ruby/bin
-fish_add_path --path $homebrew/opt/php@8.1/bin
-fish_add_path --path $homebrew/opt/node@22/bin
-fish_add_path --path /usr/local/bin
-fish_add_path --path ~/.cargo/bin
-fish_add_path --path ~/.dotnet
-fish_add_path --path ~/.dotnet/tools
-fish_add_path --path ~/.deno/bin
-fish_add_path --path ~/.local/bin
-fish_add_path --path ~/.dotfiles/bin
+fish_add_path --move --path $homebrew/sbin
+fish_add_path --move --path $homebrew/bin
+fish_add_path --move --path $homebrew/opt/ruby/bin
+fish_add_path --move --path $homebrew/opt/php@8.1/bin
+fish_add_path --move --path $homebrew/opt/node@22/bin
+fish_add_path --move --path /usr/local/bin
+fish_add_path --move --path ~/.cargo/bin
+fish_add_path --move --path ~/.dotnet
+fish_add_path --move --path ~/.dotnet/tools
+fish_add_path --move --path ~/.deno/bin
+fish_add_path --move --path ~/.local/bin
+fish_add_path --move --path ~/.dotfiles/bin
