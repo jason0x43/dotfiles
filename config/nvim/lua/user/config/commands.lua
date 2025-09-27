9beea6470ffcbf54a0321219dfefa63d84f62448
Jason Cheatham <jason@jasoncheatham.com>
2025-09-27 11:03:57 -0600

    Update user commands to use mini pickers

--- config/nvim/lua/user/config/commands.lua
+++ config/nvim/lua/user/config/commands.lua
@@ -1,51 +1,33 @@
-if vim.fn.executable('tig') == 1 then
-  vim.api.nvim_create_user_command('Tig', function()
-    require('snacks').terminal('tig', {
-      win = {
-        border = 'rounded',
-      },
-    })
-  end, { desc = 'Open tig' })
-end
-
-vim.api.nvim_create_user_command('Autocommands', function()
-  require('snacks').picker.autocmds()
-end, { desc = 'List configured autocommands' })
-
 vim.api.nvim_create_user_command('Commands', function()
-  require('snacks').picker.commands()
+  MiniPick.registry.commands()
 end, { desc = 'List configured user commands' })
 
 vim.api.nvim_create_user_command('Config', function()
-  require('snacks').picker.explorer({ cwd = '~/.config' })
+  MiniFiles.open('~/.config')
 end, { desc = 'Open an explorer in the config directory' })
 
 vim.api.nvim_create_user_command('Highlights', function()
-  require('snacks').picker.highlights()
+  MiniPick.registry.hl_groups()
 end, { desc = 'List highlight groups' })
 
 vim.api.nvim_create_user_command('Icons', function()
-  require('snacks').picker.icons()
+  MiniPick.registry.icons()
 end, { desc = 'List available icons' })
 
 vim.api.nvim_create_user_command('Keys', function()
-  require('snacks').picker.keymaps()
+  MiniPick.registry.keymaps()
 end, { desc = 'List configured keymaps' })
-
-vim.api.nvim_create_user_command('Lsps', function()
-  require('snacks').picker.lsp_config()
-end, { desc = 'List known language servers' })
 
 vim.api.nvim_create_user_command('Map', function()
   require('mini.map').toggle()
 end, { desc = 'Show or hide a file map' })
 
 vim.api.nvim_create_user_command('Modified', function()
-  require('snacks').picker.git_status()
+  MiniPick.registry.modified()
 end, { desc = 'List modified git files' })
 
 vim.api.nvim_create_user_command('Notifications', function()
-  require('snacks').notifier.show_history()
+  MiniNotify.show_history()
 end, { desc = 'Show a list of displayed notifications' })
 
 vim.api.nvim_create_user_command('Recent', function()
