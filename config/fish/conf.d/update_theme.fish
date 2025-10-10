5c109df1bd430cd7929561673ff2ce558ba8ac15
Jason Cheatham <jason@jasoncheatham.com>
2025-10-09 22:27:30 -0600

    Remove dynamic fish theme functions

--- config/fish/conf.d/update_theme.fish
+++
@@ -1,9 +0,0 @@
-function update_theme --on-variable theme
-    if [ "$theme" = "light" ]
-        fish_config theme choose 'Catppuccin Latte'
-        set -gx LS_COLORS $(vivid generate catppuccin-latte)
-    else
-        fish_config theme choose 'Catppuccin Mocha'
-        set -gx LS_COLORS $(vivid generate catppuccin-mocha)
-    end
-end
