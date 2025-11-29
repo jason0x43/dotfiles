6bb7a027c75a99c576850cb8f3c57ee5eff79b42
Jason Cheatham <jason@jasoncheatham.com>
2025-11-28 19:46:09 -0700

    Use different mapping for fish-ai

--- config/fish/conf.d/10_env.fish
+++ config/fish/conf.d/10_env.fish
@@ -3,7 +3,6 @@ set -gx NODE_OPTIONS --disable-warning=ExperimentalWarning --enable-source-maps
 set -gx EDITOR nvim
 set -gx RIPGREP_CONFIG_PATH ~/.config/ripgrep
 set -gx COREPACK_ENABLE_AUTO_PIN 0
-set -gx FISH_AI_KEYMAP_2 ctrl-o
 set -gx DOTNET_ROOT ~/.dotnet
 set -gx XDG_CONFIG_HOME ~/.config
 set -gx XDG_DATA_HOME ~/.local/share
