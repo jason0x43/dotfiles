02d15533c60c11a0e88d604fe89bb26bf4636d40
Jason Cheatham <jason@jasoncheatham.com>
2025-11-28 19:48:13 -0700

    Make some fish bindings more like neovims

--- config/fish/conf.d/99_bindings.fish
+++ config/fish/conf.d/99_bindings.fish
@@ -1,4 +1,6 @@
 fish_vi_key_bindings
 
-bind -M insert \ce forward-char
-bind -M insert \cy forward-char
+bind -M insert ctrl-e cancel
+bind -M insert ctrl-y accept-autosuggestion
+bind -M insert ctrl-n down-or-search
+bind -M insert ctrl-p up-or-search
