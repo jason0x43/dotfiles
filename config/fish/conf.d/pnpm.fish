842650486e00cfe3f37d74431a1b3d5abc4e5452
Jason Cheatham <jason@jasoncheatham.com>
2026-07-18 11:32:56 -0600

    Update fish pnpm config

--- config/fish/conf.d/pnpm.fish
+++ config/fish/conf.d/pnpm.fish
@@ -1,4 +1,4 @@
-set -gx PNPM_HOME /Users/jason/Library/pnpm
-if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
+set -gx PNPM_HOME "/Users/jason/Library/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+  set -gx PATH "$PNPM_HOME/bin" $PATH
 end
